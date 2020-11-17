fee0964b8c8b4cb9f687ff65c8e273f760152169
ashiranka <30506843+ashiranka@users.noreply.github.com>
2020-11-16 20:21:38 -0800

    Add files via upload

---
+++ KMP.java
@@ -0,0 +1,106 @@
+package StringPlagarism;
+
+public class KMP {
+	public int searchTimeCount= 0;
+	public int preprocessingTimeCount= 0, timeCount=0;
+	
+	
+    public int[] preProcessPattern(char[] ptrn) {
+        int i = 0, j = -1;
+        int ptrnLen = ptrn.length;
+        int[] b = new int[ptrnLen + 1];
+ 
+        b[i] = j;
+        while (i < ptrnLen) {
+            while (j >= 0 && ptrn[i] != ptrn[j]) {
+                j = b[j];
+                preprocessingTimeCount++;
+            }
+            i++;
+            j++;
+            b[i] = j;
+            
+        }
+        //print pettern, partial match table and index
+        System.out.println("printing pattern, partial match table, and its index");
+        System.out.print(" ");
+        for (char c : ptrn) {
+            System.out.print(c + "   ");
+        }
+        System.out.println(" ");
+        for (int tmp : b) {
+            System.out.print(tmp + "   ");
+        }
+        System.out.print("\n ");
+        for (int l = 0; l < ptrn.length; l++) {
+            System.out.print(l + "   ");
+        }
+        System.out.println(b);
+        return b;
+    }
+    public int searchSubString(String text, String ptrn) {
+        int i = 0, j = 0;
+        // pattern and text lengths
+        String patern = ptrn;
+        String txt = text;
+        char[] patrn = patern.toCharArray();
+        char[] t = txt.toCharArray();
+        
+        int ptrnLen = patrn.length;
+        int txtLen = t.length;
+        int maxSubstringLength=0;
+ 
+        // initialize new array and preprocess the pattern
+        int[] b = preProcessPattern(patrn);
+ 
+        while (i < txtLen) {
+            while (j >= 0 && t[i] != patrn[j]) {
+               System.out.println("Mismatch happened, between text char "
+                        + t[i] + " and pattern char " + patrn[j]
+                        + ", \nhence jumping the value of " + "j from " + j
+                        + " to " + b[j] + " at text index i at " + i
+                        + " based on partial match table");
+                j = b[j];
+               
+           searchTimeCount++;  
+            }
+            i++;
+            j++;
+            maxSubstringLength = Math.max(maxSubstringLength, j);
+            System.out.println("The value of J is "+ j);
+ 
+            // a match is found
+            if (j == ptrnLen) {
+                System.out.println("FOUND SUBSTRING AT i " + i + " and index:"
+                        + (i - ptrnLen));
+                System.out.println("Setting j from " + j + " to " + b[j]);
+                j = b[j];
+            }
+        }
+        //System.out.println("The Length of Max Substring Length is " + maxSubstringLength);
+        return maxSubstringLength;
+    }
+    public int getTimeCount(){
+    	if(searchTimeCount>preprocessingTimeCount)
+    	{ timeCount = searchTimeCount;
+    	}
+    	else
+    	{
+    		timeCount = preprocessingTimeCount;
+    	}
+        return timeCount;
+}
+    // only for test purposes
+
+	public static void main(String[] args) {
+		KMP stm = new KMP();
+        // pattern
+        String patern,text;
+        text ="Shreyas";
+        patern = "Shreya";
+        stm.searchSubString(text, patern);
+		
+
+	}
+
+}
---
+++ Naive.java
@@ -0,0 +1,50 @@
+package StringPlagarism;
+
+public class Naive {
+public int timeCount=0;
+	
+	public int search(String pat, String txt) {
+		int patternLen = pat.length();
+		int txtLen = txt.length();
+		int maxcount=0;
+		
+		for (int i = 0; i <= txtLen - patternLen; i++)
+	    {
+			int j;
+
+			/* For current index i, check for pattern match */
+			for (j = 0; j < patternLen; j++) {
+				
+				timeCount++;
+				if (txt.charAt(i + j) != pat.charAt(j))
+				{
+					//System.out.println("J is"+ j);
+					maxcount= Math.max(maxcount,j);
+					break;
+				}
+				
+			}
+			if (j == patternLen) // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
+			{
+				//System.out.println("Pattern found at index \n" + (i+1));
+				i = i + patternLen;
+				maxcount=patternLen;
+			}
+		}
+		//System.out.println("The length of the maxmium substring is " +maxcount);
+		return maxcount;
+	}
+	public int getTimeCount(){
+        return timeCount;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				String txt = "prakash is shreyas father";
+				String pat = "father";
+				Naive a=new Naive();
+				a.search(pat,txt);
+
+	}
+
+}
